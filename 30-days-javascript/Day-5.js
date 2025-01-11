8f5bb10703909d6f7bebff060626f0330b322c48
DifaM12 <difamuhamad06@gmail.com>
2025-01-11 16:40:20 +0700

    Day-5

--- 30-days-javascript/Day-5.js
+++ 30-days-javascript/Day-5.js
@@ -3,4 +3,10 @@
  * @param {Function} fn
  * @return {number[]}
  */
-var map = function (arr, fn) {};
+var map = function (arr, fn) {
+  const newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArr[i] = fn(arr[i], i);
+  }
+  return newArr;
+};
